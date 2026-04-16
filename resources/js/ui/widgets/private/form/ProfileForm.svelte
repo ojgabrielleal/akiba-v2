9fa1e1976ac053bf8a45d1c1618febc9253b248e
ojgabrielleal <joaogabrieel79@gmail.com>
2026-04-15 21:44:56 -0300

    feat: implement ProfileForm Svelte component and update build manifest

--- resources/js/ui/widgets/private/form/ProfileForm.svelte
+++ resources/js/ui/widgets/private/form/ProfileForm.svelte
@@ -208,7 +208,6 @@
                         name="likes"
                         class="w-full h-12 bg-neutral-aurora font-noto-sans rounded-lg outline-none pl-4"
                         bind:value={item.content}
-                        required
                     >
                         {#each userPreferences as item}
                             <option value={item.value}>{item.name}</option>
