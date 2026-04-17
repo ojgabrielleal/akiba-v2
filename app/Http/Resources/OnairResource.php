826419656410ea74bc768db66781734c07febbe9
ojgabrielleal <joaogabrieel79@gmail.com>
2026-04-16 22:23:56 -0300

    feat: create OnairResource to transform on-air program data for API responses

--- app/Http/Resources/OnairResource.php
+++ app/Http/Resources/OnairResource.php
@@ -34,7 +34,7 @@ class OnairResource extends JsonResource
                 ],
             ],
             'current_song' => $this?->current_song,
-            'created_at' => $this->created_at->setTimezone('America/Sao_Paulo')->format('d/m/Y H:i:s'),
+            'created_at' => $this->created_at->setTimezone('America/Sao_Paulo')->format('d/m/Y H:i'),
         ];  
     }
 }
