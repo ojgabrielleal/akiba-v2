610d411c1d59ac1fedc33832a6fba4436f3a27d2
ojgabrielleal <joaogabrieel79@gmail.com>
2026-05-24 23:29:58 -0300

    Add decoration and texture JSON files for player customization
    - Created decoration.json to define various player decorations, including an active "rains" decoration and multiple disabled placeholders.
    - Created texture.json to define available textures for players, including an active texture and a disabled placeholder.

--- resources/js/data/index.js
+++ resources/js/data/index.js
@@ -1,5 +1,7 @@
 export { default as calendarTags } from "./calendar/tag.json";
 export { default as locutionIcons } from "./locution/icon.json";
+export { default as locutionTextures } from "./locution/texture.json";
+export { default as locutionDecorations } from "./locution/decoration.json";
 export { default as postTags } from "./post/tag.json";
 export { default as userPreferences } from "./user/preference.json";
 export { default as navbar } from "./default/navbar.json";
